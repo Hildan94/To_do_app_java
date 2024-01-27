067e850e0805ecedd3099f340bd54cbe727186e7
HildibjørgDidriksen <s164539@student.dtu.dk>
2024-01-27 22:43:46 +0100

    updated request

--- src/main/java/org/example/to_do_list/web/ToDoController.java
+++ src/main/java/org/example/to_do_list/web/ToDoController.java
@@ -1,5 +1,8 @@
 package org.example.to_do_list.web;
 
+import org.example.to_do_list.domain.TodoItem;
+import org.example.to_do_list.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //listens for REST requests and sends a response
 public class ToDoController {
 
+    @Autowired
+    private TodoService todoService;
     // fetch all todo Items
     @GetMapping("/api/todoItems")
     public ResponseEntity<?> fetchAllTodoItems () {
+        List<TodoItem> todoItems = todoService.fetchAllTodoItems();
 
+        return ResponseEntity.ok(todoItems);
     }
 }
