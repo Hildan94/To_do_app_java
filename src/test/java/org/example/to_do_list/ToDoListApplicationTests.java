a42fc694bc331b3dbc636226498037bc62c13285
HildibjørgDidriksen <s164539@student.dtu.dk>
2024-01-27 22:08:37 +0100

    First commit

---
+++ src/test/java/org/example/to_do_list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
